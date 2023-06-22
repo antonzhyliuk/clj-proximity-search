bb0c94a43d3b6d0e62d7c53077491df1d3171196
Anton Žyluk <antonzhyliuk@gmail.com>
2023-06-22 18:52:47 +0300

    Add org group name to the project name

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-proximity-search "0.1.0-SNAPSHOT"
-  :description "Proximity search algorithm implementation."
+(defproject org.clojars.antonzhyliuk/clj-proximity-search "0.1.0-SNAPSHOT"
+  :description "Proximity search algorithm"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
