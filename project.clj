cee32677d8b6ecb94cd13e5ef3131e2dcbadd858
Anton Žyluk <antonzhyliuk@gmail.com>
2023-06-22 19:36:46 +0300

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.antonzhyliuk/clj-proximity-search "0.1.0-SNAPSHOT"
+(defproject org.clojars.antonzhyliuk/clj-proximity-search "0.1.0"
   :description "Proximity search algorithm"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/antonzhyliuk/clj-proximity-search"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
